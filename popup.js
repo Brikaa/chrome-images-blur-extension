aae5fa50623630d551036193b8cae8b50a08ad9d
Omar Brikaa <brikaaomar@gmail.com>
2025-02-02 20:24:23 +0200

    Initial domain-specific sliders

--- popup.js
+++ popup.js
@@ -1,38 +1,56 @@
 const slider = document.getElementById('blurSlider');
-
-const globalCheckbox = document.getElementById('globalCheckbox');
-const domainSpecificCheckbox = document.getElementById('domainCheckbox');
-const allDomainsDiv = document.getElementById('allDomainsDiv');
+const domainSlider = document.getElementById('domainBlurSlider');
+const domainDiv = document.getElementById('domainSpecific');
 const domainLabel = document.getElementById('domain');
+const domainCheckbox = document.getElementById('domainCheckbox');
 
-const toggleDomainSpecificCheckbox = () => {
-  if (globalCheckbox.checked) {
-    allDomainsDiv.style.display = 'none';
+const setDomainDiv = () => {
+  if (domainCheckbox.checked) {
+    domainDiv.style.display = 'initial';
   } else {
-    allDomainsDiv.style.display = 'initial';
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      if (tabs.length > 0) {
-        domainLabel.innerText = tabs[0].url ?? tabs[0].pendingUrl;
-      } else {
-        domainLabel.innerText = 'unknown';
-      }
-      domainLabel.innerText = domainLabel.innerText.split('/')[0];
-    });
+    domainDiv.style.display = 'none';
   }
 };
 
-globalCheckbox.addEventListener('change', () => {
-  chrome.storage.local.set({ global: globalCheckbox.checked });
-  toggleDomainSpecificCheckbox();
-});
+(async () => {
+  // Get domain name, and populate domain label
+  let domainName = null;
+  const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
+  if (tabs.length > 0) {
+    domainName = tabs[0].url ?? tabs[0].pendingUrl;
+    domainName = domainName.replace('https://', '').replace('http://', '').split('/')[0];
+    domainLabel.innerText = domainName;
+  }
 
-chrome.storage.local.get(['blur', 'global'], (result) => {
-  slider.value = result.blur ?? 0;
-  globalCheckbox.checked = result.global || result.global === undefined;
-  toggleDomainSpecificCheckbox();
-});
+  // On domain-specific checkbox change, show/hide the domain-specific slider and store/remove the domain-specific value
+  domainCheckbox.addEventListener('change', () => {
+    setDomainDiv();
+    if (domainCheckbox.checked) {
+      chrome.storage.local.set({ [domainName]: domainSlider.value });
+    } else {
+      chrome.storage.local.remove([domainName]);
+    }
+  });
 
-slider.addEventListener('input', (e) => {
-  const value = e.target.value;
-  chrome.storage.local.set({ blur: value });
-});
+  // On init, set global slider to value in storage or 0
+  // + check domainCheckbox if needed, toggle domain slider, and set domain-specific slider value
+  chrome.storage.local.get(['blur', domainName], (result) => {
+    slider.value = result.blur ?? 0;
+    if (result[domainName]) {
+      domainCheckbox.checked = true;
+      setDomainDiv();
+      domainSlider.value = result[domainName];
+    }
+  });
+
+  // On sliders change set the appropriate local storage values
+  slider.addEventListener('input', (e) => {
+    const value = e.target.value;
+    chrome.storage.local.set({ blur: value });
+  });
+
+  domainSlider.addEventListener('input', (e) => {
+    const value = e.target.value;
+    chrome.storage.local.set({ [domainName]: value });
+  });
+})();
